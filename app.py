aa1c12584c3d6b1b38dc8035d61f0dafb1919f59
dileep-jangam72 <108661931+dileep-jangam72@users.noreply.github.com>
2025-10-02 17:13:54 +0000

    code update

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello, People ! This is a DevOps demo app running on Kubernetes!"
-
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
